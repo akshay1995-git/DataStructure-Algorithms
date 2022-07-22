986cdc246436ec7861c76e6b620e4534816f6f52
Akshay <iamtheak1995@gmail.com>
2022-07-22 07:44:10 +0530

    Adj Matrix in graph committed

---
+++ 10.Graph/src/Adjacency_Matrix/GraphNode.java
@@ -0,0 +1,12 @@
+package Adjacency_Matrix;
+
+public class GraphNode {
+String name;
+int index;
+boolean isVisited=false;
+//public Object neighbors;
+public GraphNode(String nm,int ind) {
+	name=nm;
+	index=ind;
+}
+}

3866b35c8c60e3bbcd231b99093ad428a62e4fe8
Akshay <iamtheak1995@gmail.com>
2022-07-28 08:12:34 +0530

    merge sort committed

--- 9.Sorting/src/Sort_Algorithms/Merge_Sort.java
+++ 9.Sorting/src/Sort_Algorithms/Merge_Sort.java
@@ -1,23 +1,31 @@
 package Sort_Algorithms;
-
+//Merge Sort :
+/*
+This sort use divide and conquere algorithm in which start dividing array in subarray
+and then recursivley sort each of them
+Time complexity :O(nlogn)
+Space Complexity :O(n)
+USES :inversion counting problem,sorting,E-commerce application
+*/
 
 public class Merge_Sort {
 public void merge(int []array,int left,int middle,int right) {
 	int[] leftArr=new int[middle-left+1];
 	int []rightArr=new int[right-middle];
 	for(int i=0;i<middle-left;i++) {
-		leftArr[i]=array[i];
+		leftArr[i]=array[i];//left sub to leftArr
 		//System.out.print(" "+leftArr[i]);
 	}
 	
 	for(int i=middle+1;i<right-middle;i++) {
-		rightArr[i]=array[i];
+		rightArr[i]=array[i];//right sub to rightArr
 		//System.out.println("  "+rightArr[i]);
 	}
 
 	int i=0;
 	int j=0;
 	int k=left;
+        //assign left and rightArr valye to array by comparison
 	while(i<leftArr.length && j<rightArr.length){
 		if(leftArr[i]<=rightArr[j]) {
 			array[k]=leftArr[i];
@@ -29,6 +37,7 @@ public void merge(int []array,int left,int middle,int right) {
 		}
 	k++;
 	}
+       //assign remaining value toarray
 	
 	 while (i <leftArr.length) {
 	        array[k] = leftArr[i];
@@ -42,11 +51,12 @@ public void merge(int []array,int left,int middle,int right) {
 	        k++;
 	    }
 }
+//Method to sort array
 public void merge_sort(int []array,int left,int right) {
 	if(right>left) {
 		int mid=(left+right)/2;
-		merge_sort(array,left,mid);
-		merge_sort(array,mid+1,right);
+		merge_sort(array,left,mid);//left sub array
+		merge_sort(array,mid+1,right);//right sub array
 		merge(array,left,mid,right);
 	}
 }
